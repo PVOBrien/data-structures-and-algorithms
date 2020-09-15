7e0dac36b23031a0eaa9f5d9cc5788e5b36e81e4
Paul OBrien <PVOVideo@outlook.com>
2020-09-14 18:32:35 -0700

    create working arrayReverse.java function

--- java-code-challenges/src/main/java/arrayReverse.java
+++ java-code-challenges/src/main/java/arrayReverse.java
@@ -1,11 +1,20 @@
-public class arrayReverse {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
-    }
-}
+public class ArrayReverse {   
+  public static void main(String[] args) {
+    int[] original = new int[]{89, 2354, 3546, 23, 10, -923, 823, -12};
 
-class numberReverser {
-    public Object arrayReverse(int [] array) {
-        int arr[] = new int[].length.array;
+    int[] theReversed = arrayReverse(original);
+
+    System.out.println(Arrays.toString(theReversed));
+
+  }
+    public static int[] arrayReverse(int[] original) {
+      int theLength = original.length;
+      int[] reversed = new int[theLength];
+      for (int i = 0; i < original.length; i++){
+        reversed[original.length - i - 1] = original[i];
+      } 
+      return reversed;
     }
-}
+}       
