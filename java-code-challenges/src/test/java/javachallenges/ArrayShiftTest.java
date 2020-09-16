d8bbead1e150fb8c6c2d54ac6d4d3113d5f00ae9
Paul OBrien <PVOVideo@outlook.com>
2020-09-16 09:31:57 -0700

    clean up intro code

--- java-code-challenges/src/test/java/javachallenges/ArrayShiftTest.java
+++ java-code-challenges/src/test/java/javachallenges/ArrayShiftTest.java
@@ -7,11 +7,6 @@ import static org.junit.Assert.assertTrue;
 
 public class ArrayShiftTest {
     @Test
-    public void testSomeLibraryMethod() {
-        Library classUnderTest = new Library();
-        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
-    }
-    @Test
     public void arrayShiftTest() {
         ArrayShift theWorking = new ArrayShift();
         int[] originalArray = {1, 2, 3, 4};
