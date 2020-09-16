ca813ecf0c9c8f6db23d1b77a36153f65d2d4fe8
Paul OBrien <PVOVideo@outlook.com>
2020-09-16 09:34:09 -0700

    clean up code

--- java-code-challenges/src/test/java/javachallenges/ArrayShiftTest.java
+++ java-code-challenges/src/test/java/javachallenges/ArrayShiftTest.java
@@ -3,7 +3,6 @@ package javachallenges;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class ArrayShiftTest {
     @Test
