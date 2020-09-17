de18d5ca895acb1a19bee6784dc5d38062342d9c
Paul OBrien <PVOVideo@outlook.com>
2020-09-16 20:58:08 -0700

    catch up files

--- java-code-challenges/src/main/java/javachallenges/BinarySearch.java
+++ java-code-challenges/src/main/java/javachallenges/BinarySearch.java
@@ -1,4 +1,28 @@
 package javachallenges;
 
 public class BinarySearch {
+  public int binarySearch(int[] arrayToSearch, int searchKey){
+    int low = 0;
+    int high = arrayToSearch.length - 1;
+    int middle = (low + high) /2;
+
+//    if (searchKey<arrayToSearch[low] || searchKey>arrayToSearch[high]){
+//      return -1; //this returns if the search key is not between the min and max values of the array
+//    }
+
+    while (0 <= 4) {
+      if(searchKey == arrayToSearch[middle]){
+        System.out.println(middle);
+        return middle;
+      }
+
+      if (arrayToSearch[middle] < searchKey) {
+        low = middle + 1;
+      } else {
+        high = middle - 1;
+      }
+      return middle = -1;
+    }
+//    return middle;
+  }
 }
--- java-code-challenges/src/test/java/javachallenges/BinarySearchTest.java
+++ java-code-challenges/src/test/java/javachallenges/BinarySearchTest.java
@@ -3,5 +3,13 @@ package javachallenges;
 import org.junit.Test;
 
 public class BinarySearchTest {
-  
+  @Test
+  public void BinarySearching() {   // function takes in an array and a search key
+    BinarySearch bananaSearch = new BinarySearch();
+    int[] searchInput = {4, 8, 15, 16, 23, 42};
+    int searchKey = 15;
+
+    System.out.println(bananaSearch.binarySearch(searchInput,searchKey));
+
+  }
 }
