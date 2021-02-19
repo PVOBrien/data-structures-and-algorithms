52cf1a0df1301da5614086a85b51ba31c46a8fe0
Paul OBrien <PVOVideo@outlook.com>
2021-02-18 20:45:30 -0800

    save point

--- java-code-challenges/src/main/java/codeChallenges/PersistenceToOne.java
+++ java-code-challenges/src/main/java/codeChallenges/PersistenceToOne.java
@@ -1,10 +1,5 @@
 package codeChallenges;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 public class PersistenceToOne {
 
     public static int persistence(long n) {
