5efe1b89a093a475f4bd8720e2a0719ff4e9f1a7
Paul OBrien <PVOVideo@outlook.com>
2021-03-06 19:05:21 -0800

    save point

--- java-code-challenges/src/main/java/codeChallengesLeet/AppleCollector.java
+++ java-code-challenges/src/main/java/codeChallengesLeet/AppleCollector.java
@@ -1,44 +1,50 @@
 package codeChallengesLeet;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class AppleCollector { // https://leetcode.com/problems/minimum-time-to-collect-all-apples-in-a-tree/
-
-    HashMap<Integer, int[]> appleRoutes = new HashMap<>();
     HashSet<int[]> visitedRoutes = new HashSet<>();
-    HashSet<int[]> routeWApple = new HashSet<>();
+    ArrayList<int[]> currentPath = new ArrayList<>();
+    HashSet<String> routeWApple = new HashSet<>();
+    int[][] globalEdges;
 
     public int minTime(int n, int[][] edges, List<Boolean> hasApple) {
         int initialCount = (int) hasApple.stream().filter(apple -> apple).count();
         if (initialCount == 0 || hasApple.get(0)) return initialCount;
 
-        for (int i = 0; i < hasApple.size(); i++) {
-            int edgeFrom = edges[i][1]; // or goingto but...
-            for (int j = 0; j < hasApple.size(); j++) {
-                if (edges[j][0] == edgeFrom) {
-                    if (hasApple.get(j)) {
-                        routeWApple.add(edges[i]);
-                        routeWApple.add(edges[j]);
-                    }
-                    // TODO: recursive helper function to track when the deepest node/hasApple[index] is true, then add to hashset both "going from" and "going to" edges.
+        globalEdges = edges;
+        for (int i = 0; i < globalEdges.length; i++) {
+            if (!visitedRoutes.contains(edges[i])) {
+                visitedRoutes.add(edges[i]);
+                applePathFinder(currentPath, edges[i], hasApple, n);
+            }
+        }
+        return routeWApple.size();
+    }
+
+    public boolean applePathFinder(ArrayList<int[]> followedPath, int[] thisEdge, List<Boolean> hasApple, int n) {
+        boolean isApple = false;
+        if (thisEdge[1] < n) {
+            followedPath.add(thisEdge);
+            for (int i = 0; i < globalEdges.length; i++) {
+                if (globalEdges[i][0] == thisEdge[1]) {
+                    isApple = applePathFinder(followedPath, globalEdges[i], hasApple, n);
                 }
             }
         }
-        return 0;
+        if (hasApple.get(thisEdge[1])) isApple = true;
+        if (isApple) {
+            int[] tempArr = {thisEdge[1], thisEdge[0]};
+            routeWApple.add(Arrays.toString(thisEdge));
+            routeWApple.add(Arrays.toString(tempArr));
+        }
+        System.out.println();
+        routeWApple.forEach(item -> System.out.println(Arrays.toString(item)));
+        return isApple;
     }
-    private static boolean applePathFinder(int[][] followedPath, int[] pathTo) {
-        boolean apple = false;
-
-
-
-        return apple;
-    }
-
 }
-
-
 //  Input: n = 7, edges = [[0,1],[0,2],[1,4],[1,5],[2,3],[2,6]], hasApple = [false,false,true,false,true,true,false]
 //  Output: 8
