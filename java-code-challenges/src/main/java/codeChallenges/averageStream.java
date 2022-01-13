b52a9e3e0bc26a5e989e8c31cb1669c7b55b1495
Paul OBrien <pvovideo@outlook.com>
2022-01-12 21:46:12 -0800

    catch up

--- java-code-challenges/src/main/java/codeChallenges/averageStream.java
+++ java-code-challenges/src/main/java/codeChallenges/averageStream.java
@@ -1,2 +1,13 @@
-package codeChallenges;public class averageStream {
+package codeChallenges;
+
+import java.util.Arrays;
+import java.util.OptionalDouble;
+
+public class averageStream {
+    public static boolean betterThanAverage(int[] classPoints, int yourPoints) {
+        System.out.println(Arrays.toString(classPoints));
+        System.out.println(yourPoints);
+        OptionalDouble theAverage = Arrays.stream(classPoints).average();
+        return yourPoints > theAverage.getAsDouble();
+    }
 }
--- java-code-challenges/src/main/java/hackerRankChallenges/SockPairs.java
+++ java-code-challenges/src/main/java/hackerRankChallenges/SockPairs.java
@@ -1,5 +1,6 @@
 package hackerRankChallenges;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
